30d129f0b36c68aa7be39b432c08e788db4fc2e1
xluxx <125575752+xluxx@users.noreply.github.com>
2024-11-26 19:02:19 +0300

    Add files via upload

---
+++ dicts.py
@@ -0,0 +1,22 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+a = [5, 3, 3, 5, 4]
+avg_1 = sum(a) / len(a)
+b = [2, 2, 2, 3]
+avg_2 = sum(b) / len(b)
+c = [4, 5, 5, 2]
+avg_3 = sum(c) / len(c)
+d = [4, 4, 3]
+avg_4 = sum(d) / len(d)
+i = [5, 5, 5, 4, 5]
+avg_5 = sum(i) / len(i)
+students_1 = 'Aaron - ' + str(avg_1)
+print(students_1)
+students_2 = 'Bilbo - ' + str(avg_2)
+print(students_2)
+students_3 = 'Johnny - ' + str(avg_3)
+print(students_3)
+students_4 = 'Khendrik - ' + str(avg_4)
+print(students_4)
+students_5 = 'Steve - ' + str(avg_5)
+print(students_5)
